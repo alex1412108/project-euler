5bb0008e925ad15ea270bc87e6c70e8ad741603e
alex1412108 <alex1412108@gmail.com>
2014-01-28 17:57:12 +0000

    solve the problem

--- 0005.py
+++ 0005.py
@@ -1,3 +1,17 @@
 #2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 #What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 #from:http://projecteuler.net/problem=5
+print "started"
+cutoff=0
+num=0
+divisors=20
+while cutoff<divisors-2:
+	num+=1
+	cutoff=0
+	for i in range(2,divisors):
+		if num%i==0:
+			cutoff+=1
+		else:
+			break
+print i
+print num
