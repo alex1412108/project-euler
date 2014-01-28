4b8e2ea3d14a01a31a79999bcb2362a20ca4ce2a
alex1412108 <alex1412108@gmail.com>
2014-01-28 22:09:30 +0000

    solve problem 6

--- 0006.py
+++ 0006.py
@@ -1,7 +1,15 @@
 #The sum of the squares of the first ten natural numbers is,
-#12 + 22 + ... + 102 = 385
+#1^2 + 2^2 + ... + 10^2 = 385
 #The square of the sum of the first ten natural numbers is,
-#(1 + 2 + ... + 10)2 = 552 = 3025
-#Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+#(1 + 2 + ... + 10)^2 = 55^2 = 3025
+#Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
 #Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 #from:http://projecteuler.net/problem=6
+sumofs=0
+sofsum=0
+for i in range(1,101):
+	sumofs+=i**2
+	sofsum+=i
+sofsum=sofsum**2
+num=sofsum-sumofs
+print num
