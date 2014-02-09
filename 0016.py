341bd0d86dcc3d64ed9b192ea9612990b72514ee
alex1412108 <alex1412108@gmail.com>
2014-02-09 02:34:09 +0000

    solve problem 16

--- 0016.py
+++ 0016.py
@@ -1,2 +1,8 @@
-#215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
-#What is the sum of the digits of the number 21000?
+#2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+#What is the sum of the digits of the number 2^1000?
+
+num=0
+numlist=list(str(2**1000))
+for i in numlist:
+	num+=int(i)
+print num
