93077f7a677065f3705c11c7f3c0e35e38e7f775
alex1412108 <alex1412108@gmail.com>
2014-02-09 02:47:35 +0000

    create function of values for the letters

--- 0017.py
+++ 0017.py
@@ -2,3 +2,41 @@
 #If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
 #NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters.
 #The use of "and" when writing out numbers is in compliance with British usage.
+
+def main():
+	
+
+
+def f(x):
+    return {
+        1: 'ONE',
+        2: 'TWO',
+        3: 'THREE',
+        4: 'FOUR',
+        5: 'FIVE',
+        6: 'SIX',
+        7: 'SEVEN',
+        8: 'EIGHT'
+        9: 'NINE'
+        10:'TEN',
+        11:'ELEVEN',
+        12:'TWELVE',
+        13:'THIRTEEN',
+        14:'FOURTEEN',
+        15:'FIFTEEN',
+        16:'SIXTEEN',
+        17:'SEVENTEEN',
+        18:'EIGHTEEN',
+        19:'NINETEEN',
+        20:'TWENTY',
+        30:'THIRTY',
+        40:'FOURTY',
+        50:'FIFTY',
+        60:'SIXTY',
+        70:'SEVENTY',
+        80:'EIGHTY',
+        90:'NINETY',
+        100:'HUNDRED',
+        1000: "ONETHOUSAND",
+        }.get(x, 0) 
+main()
