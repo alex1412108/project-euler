29809e7e8179f6de9428848a3bb31cb7c3659270
alex1412108 <alex1412108@gmail.com>
2014-02-09 13:47:48 +0000

    solve problem 20

--- 0020.py
+++ 0020.py
@@ -1,4 +1,12 @@
-#n! means n × (n − 1) × ... × 3 × 2 × 1
-#For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+#n! means n * (n - 1) * ... * 3 * 2 * 1
+#For example, 10! = 10 * 9 * ... * 3 * 2 * 1 = 3628800,
 #and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 #Find the sum of the digits in the number 100!
+
+import math
+ 
+num=0
+numlist=list(str(math.factorial(100)))
+for i in numlist:
+	num+=int(i)
+print num
