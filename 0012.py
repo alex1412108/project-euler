10b19e4993276a1d95b0ea7c0ef5544a1a2d1c4e
alex1412108 <alex1412108@gmail.com>
2014-02-17 00:52:35 +0000

    solution for problem if only prime divisors wanted

--- 0012.py
+++ 0012.py
@@ -11,3 +11,33 @@
 #We can see that 28 is the first triangle number to have over five divisors.
 #What is the value of the first triangle number to have over five hundred divisors?
 #from:http://projecteuler.net/problem=12
+
+def main():
+	sumnum=0
+	num=0
+	primelist=[]
+	for i in range(1,500):#upper range needs to be 1 more than the intended as need to start at 1 instead of 0
+		num+=i
+		primelist[:]=[]
+		primes=gen_primes(num)
+		for prime in primes:
+			if num%prime==0:
+				primelist.append(prime)
+		print primelist
+		if len(primelist)>5:
+			print num
+			print primelist
+			exit(0)
+
+def gen_primes(rangeheight):
+	numlist = {}
+	num = 2
+	for num in range(2,rangeheight):
+		if num not in numlist:
+			yield num
+			numlist[num * num] = [num]
+		else:
+			for p in numlist[num]:
+				numlist.setdefault(p + num, []).append(p)
+			del numlist[num]
+main()
