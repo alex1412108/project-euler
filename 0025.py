931eb18dec15aaaf9b9662e1d7258f59ad1ae0f0
alex1412108 <alex1412108@gmail.com>
2014-02-18 17:42:52 +0000

    solve problem 25

--- 0025.py
+++ 0025.py
@@ -16,3 +16,11 @@
 #The 12th term, F_12, is the first term to contain three digits.
 #What is the first term in the Fibonacci sequence to contain 1000 digits?
 #from: http://projecteuler.net/problem=25
+numlist=[1]
+count=2
+for i in range(1,10000):
+	numlist.append(numlist[i-1]+numlist[i-2])
+	count+=1
+	if len(str(numlist[i]))>=1000:
+		print count
+		exit(0)
